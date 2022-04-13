dfe058ea31df8da50e8eb54794b9495f0f578495
Amit prajapati <57350935+IsAmitprajapati@users.noreply.github.com>
2022-04-13 22:01:50 +0530

    when click on the Navigation
    content will be change

--- Javascript.js
+++ Javascript.js
@@ -4,6 +4,11 @@ let GrowwProduct_MutualFundsDetails = document.querySelector(".GrowwProduct_Mutu
 let GrowwProduct_UsStocksDetails = document.querySelector(".GrowwProduct_UsStocksDetails");
 let GrowwProduct_FDsDetails = document.querySelector(".GrowwProduct_FDsDetails");
 
+/***change variable */
+let ChangeImageAccordingNavigation = document.querySelector(".ChangeImageAccordingNavigation");
+let ZeroText = document.querySelector(".ZeroText");
+let InsideContent = document.querySelector(".InsideContent");
+let stocksDetails_Text = document.querySelector(".stocksDetails_Text");
 
 GrowwProduct_StocksDetails.addEventListener("click",()=>{
     NavbarUnderlineActive.style.width  = "200px";
@@ -15,16 +20,33 @@ GrowwProduct_StocksDetails.addEventListener("click",()=>{
     document.querySelector(".MutualFundActive").classList.remove("Active2")
     document.querySelector(".UsStockActive").classList.remove("Active3");
     document.querySelector(".FDActive").classList.remove("Active4");
+
+    /**Change Content when Click */
+    
+    ChangeImageAccordingNavigation.src = "/ImageNavigation/stocks.png";
+    ZeroText.textContent = "Zero";
+    InsideContent.textContent = "account charges";
+    stocksDetails_Text.textContent = "You don't have to pay  a single rupee for opening a stocks account or account maintenance."
+
 })
 GrowwProduct_MutualFundsDetails.addEventListener("click",()=>{
     NavbarUnderlineActive.style.width  = "250px";
     NavbarUnderlineActive.style.transform = "translate(-650px,60px)";
    document.querySelector(".MutualFundActive").classList.add("Active2")
 
+
     /*remove*/
    document.querySelector(".StockActive").classList.remove("Active1")
    document.querySelector(".UsStockActive").classList.remove("Active3");
    document.querySelector(".FDActive").classList.remove("Active4");
+
+    /**Change Content when Click */
+    ChangeImageAccordingNavigation.src = "/ImageNavigation/MutualFunds.png";
+    ZeroText.textContent = "0%";
+    InsideContent.textContent = "commission";
+    stocksDetails_Text.textContent = "Select from 5000+ direct mutual funds and get higher return than regular funds."
+
+
 })
 GrowwProduct_UsStocksDetails.addEventListener("click",()=>{
     NavbarUnderlineActive.style.width  = "200px";
@@ -36,6 +58,14 @@ GrowwProduct_UsStocksDetails.addEventListener("click",()=>{
     document.querySelector(".MutualFundActive").classList.remove("Active2")
     document.querySelector(".StockActive").classList.remove("Active1")
     document.querySelector(".FDActive").classList.remove("Active4");
+
+    /**Change Content when Click */
+    ChangeImageAccordingNavigation.src = "/ImageNavigation/USStock.png";
+    ZeroText.textContent = "Free";
+    InsideContent.textContent = "account opening";
+    stocksDetails_Text.textContent = "Invest in Apple, Google, Netflix and many more US companies that you love without any brokerage fee."
+
+
 })
 GrowwProduct_FDsDetails.addEventListener("click",()=>{
     NavbarUnderlineActive.style.width  = "200px";
@@ -46,4 +76,13 @@ GrowwProduct_FDsDetails.addEventListener("click",()=>{
     document.querySelector(".MutualFundActive").classList.remove("Active2")
     document.querySelector(".StockActive").classList.remove("Active1")
     document.querySelector(".UsStockActive").classList.remove("Active3");
+
+    /**Change Content when Click */
+    ChangeImageAccordingNavigation.src = "/ImageNavigation/fd.png";
+    ZeroText.textContent = "6.7%";
+    InsideContent.textContent = "interest rate";
+    stocksDetails_Text.textContent = "Open fixed deposit in any bank with higher interest rates without opening a bank account."
+
+
+
 })
